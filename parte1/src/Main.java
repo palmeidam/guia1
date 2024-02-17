bb18201eb8537d98d14012ba72050f4482dce753
Pablo Almeida <palmeid95127@universidadean.edu.co>
2024-02-17 12:59:02 -0500

    Cambia config Git

--- parte1/src/Main.java
+++ parte1/src/Main.java
@@ -5,9 +5,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //Tienda tienda = new Tienda("perro" ,"perro", 15, TipoProducto.LACTEO,new Date(),"Perro",10,new Float(10));
-        //System.out.println("Hello world!"  + proyectil.getVelocidad());
     }
+
 
 
 }
